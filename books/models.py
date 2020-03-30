010f2bbea58bbf3cca2fb54efc99765a48e32cc7
Manumanglam <manurockmanglam@gmail.com>
2020-03-30 17:47:25 +0530

    reviews

--- books/models.py
+++ books/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 import uuid
-
+'''use python manage.py reset appname to delete db'''
 # Create your models here.
 class Book(models.Model):
     id=models.UUIDField(
@@ -17,3 +18,17 @@ class Book(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('book_detail',args=[str(self.id)])
+
+class Review(models.Model):
+    book=models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review=models.CharField(max_length=255)
+    author=models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.review

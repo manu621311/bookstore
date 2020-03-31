3ec5ba24a890f26cfa3c07be3e801959b57ca6f0
Manumanglam <manurockmanglam@gmail.com>
2020-03-31 11:18:29 +0530

    image_uploading

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -147,3 +147,5 @@ ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_UNIQUE_EMAIL=True
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL='admin@djangobookstore.com'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
